83e00ae3534d8d16407610c10d1e72ba7083dde5
Gabriel Aedo <aedothegabriel@gmail.com>
2025-10-10 16:16:24 -0500

    configuración correcta para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://GabrielAedoPozo.github.io',
-    base: 'confecciones_polcyr',
+  site: 'https://GabrielAedoPozo.github.io', // tu perfil de GitHub Pages
+  base: '/confecciones_polcyr/',             // el nombre del repositorio con / al inicio y final
+  outDir: 'dist',                            // carpeta donde se generará el sitio
+  build: {
+    format: 'directory',
+  },
 });
