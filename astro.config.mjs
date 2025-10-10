ba59ea162e48f35114ac9a54443c4eb9af7f0824
Gabriel Aedo <aedothegabriel@gmail.com>
2025-10-10 16:18:59 -0500

    Agregar workflow de despliegue automático

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://GabrielAedoPozo.github.io', // tu perfil de GitHub Pages
-  base: '/confecciones_polcyr/',             // el nombre del repositorio con / al inicio y final
-  outDir: 'dist',                            // carpeta donde se generará el sitio
-  build: {
-    format: 'directory',
-  },
+  site: 'https://GabrielAedoPozo.github.io',
+  base: '/confecciones_polcyr/',
 });
